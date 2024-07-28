2cbbdbdcf6a964342dda5a0843cdbb77e4dad22e
Timo Baader <timo_baader@hotmail.de>
2024-07-28 13:36:19 +0300

    adding first player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,23 +12,57 @@ public class PlayerController : MonoBehaviour
     Vector2 movementInput;
     Rigidbody2D rb;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-    // Start is called before the first frame update
+    Animator animator;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        if (movementInput != Vector2.zero) 
+        if (movementInput != Vector2.zero)
         {
-            // Check for collisions
-            int count = rb.Cast(movementInput, contactFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+            bool success = TryMove(movementInput);
 
-            if (count == 0)
+            // If we cannot move diagonally while pressing two movement keys, check if we can still move in one direction
+            // and "slide" along the obstacle
+            if (!success)
             {
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
             }
+            animator.SetBool("isMoving", success);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        // Check for collisions
+        int count = rb.Cast(
+            direction,
+            contactFilter,
+            castCollisions,
+            moveSpeed * Time.fixedDeltaTime + collisionOffset
+        );
+
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
